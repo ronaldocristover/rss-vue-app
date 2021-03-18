866b49a6970cbe49408712a8fc935e1a7326daf4
Ronaldo Cristover <ronaldochristover@gmail.com>
2021-03-19 02:19:50 +0700

    Initial Web

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,17 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">'
+    <Rss />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Rss from './components/Rss.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Rss
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
